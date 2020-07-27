29de6d26e70ce7d87c55ab40d2d62d1a7e094158
Ioan Loosley <legit.ioangogo@gmail.com>
2020-07-27 16:52:09 +0100

    main: setup the basic main structure

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import asyncio
+from contextlib import AsyncExitStack, asynccontextmanager
+from network import mqtt
+from classes.state import state_class
+from light_loop import light_loop
+from asyncio_mqtt import MqttError
+
+
+async def cancel_tasks(tasks):
+    # * This is borrowed from the example code of the mqtt libary
+    for task in tasks:
+        task.cancel()
+        try:
+            await task
+        except asyncio.CancelledError:
+            pass
+
+
+async def main(state: state_class):
+    numAttempts = 3
+    while numAttempts > 0:
+        try:
+            async with AsyncExitStack as mainStack:
+                mainTasks = set()
+                mainStack.push_async_callback(cancel_tasks, mainTasks)
+
+                mainTasks.add(asyncio.create_task(mqtt(state)))
+                mainTasks.add(asyncio.create_task(light_loop(state)))
+
+                await asyncio.gather(*mainTasks)
+        except MqttError as error:
+            print(f'Error "{error}". Reconnecting. Attempts Remaining {numAttempts}')
+        finally:
+            await asyncio.sleep(3)
